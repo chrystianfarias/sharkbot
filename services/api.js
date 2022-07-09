9c152bb0c0aa33b13b3839a6275a7638dfc19d33
Chrystian Farias <chrystian.farias@outlook.com>
2022-07-09 12:33:26 -0300

    modified:   services/api.js

--- services/api.js
+++ services/api.js
@@ -1,5 +1,5 @@
 
 const axios = require('axios');
-const api = axios.create({baseURL: 'https://api.sharkrunners.com.br/'});
+const api = axios.create({baseURL: 'https://api.sharkrunners.com.br'});
 api.defaults.headers.common['auth'] = "b6f1eb97-84ad-4156-bde2-f1e14d8e7cdf";
 module.exports = api;
