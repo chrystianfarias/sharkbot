52704c08d9779f3a3c573ddc4252eea2d75bf5a8
Chrystian Farias <chrystian.farias@outlook.com>
2022-07-09 10:36:55 -0300

    modified:   services/api.js

--- services/api.js
+++ services/api.js
@@ -1,5 +1,5 @@
 
 const axios = require('axios');
-const api = axios.create({baseURL: 'https://sharkbotapi.azurewebsites.net'});
+const api = axios.create({baseURL: 'https://sharkrunnersapi.herokuapp.com/'});
 api.defaults.headers.common['auth'] = "b6f1eb97-84ad-4156-bde2-f1e14d8e7cdf";
 module.exports = api;
